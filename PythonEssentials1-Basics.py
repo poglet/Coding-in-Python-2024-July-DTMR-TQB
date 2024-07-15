925202dd1f0ed1e94792997c1be0d1a1eb2bfecf
Chris Thomas <christopher.thomas2@my.jcu.edu.au>
2024-07-15 15:50:00 +1000

    Started Week 1 Homework

---
+++ PythonEssentials1-Basics.py
@@ -0,0 +1,32 @@
+# This are found on the Netacad Site
+
+# Module 1 - Introduction to Python and computer programming 
+
+print("Hisssssss...")
+
+
+# Module 2 - Data types, variables, basic input-output operations, basic operators 
+
+print("Hello, World!")
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+#Multiple arguments
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+# 2.2.1.1 Python literals
+
