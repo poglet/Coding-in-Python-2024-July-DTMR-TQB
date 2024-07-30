188a298dc5ebcba61dfa09768933f238f411939a
Chris Thomas <christopher.thomas2@my.jcu.edu.au>
2024-07-30 19:35:16 +1000

    Homework

---
+++ Week4-31-July-2024/module3.py
@@ -0,0 +1,56 @@
+# Python Essentials 1:
+# Module 3
+# Loops and what not
+
+
+
+
+print(2 == 2)
+print(2 == 2.)
+print(1 == 2)
+
+# These are the same:
+# black_sheep == 2 * white_sheep
+# black_sheep == (2 * white_sheep)
+
+var = 0  # Assigning 0 to var
+print(var == 0)
+
+var = 1  # Assigning 1 to var
+print(var == 0)
+
+#inequality operator
+var = 0  # Assigning 0 to var
+print(var != 0)
+
+var = 1  # Assigning 1 to var
+print(var != 0)
+
+
+n = +123
+print('Result: ', n>99)
+
+# Read two numbers
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+
+number1 = int(5)
+number2 = int(7)
+
+# Choose the larger number
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
+
+
+# x = input('Input: ')
+# if (x == 'Spathiphyllum'):
+#     print('Yes - Spathiphyllum is the best plant ever!')
+# elif(x == 'spathiphyllum'):
+#     print('No, I want a big Spathiphyllum!')
+# else: print('Spathiphyllum! Not ',x,'!')
+
---
+++ Week4-31-July-2024/quiz.py
@@ -0,0 +1,121 @@
+print('Question 1')
+
+nums = [1,2,3]
+vals = nums[-1:-2]
+
+print(nums)
+print(vals)
+
+print('Question 2')
+
+# my_list = [[0, 1, 2, 3] for i in range (2)]
+# print(my_list[2][0])
+
+print('Question 3')
+
+my_list = [3, 1, -2]
+print(my_list[my_list[-1]])
+
+print('Question 4')
+
+var = 0
+while var < 6:
+    var += 1
+    if var % 2 == 0:
+        continue
+    print("#")
+
+print('Question 5')
+
+i = 0
+while i <= 3 :
+    i += 2
+    print("*")
+
+print('Question 6')
+
+nums = [1,2,3]
+vals = nums
+del vals[1:2]
+print(nums)
+print(vals)
+
+print('Question 7')
+
+t = [[3-i for i in range (3)] for j in range (3)]
+s = 0
+for i in range(3):
+    s += t[i][i]
+print(s)
+
+print('Question 8')
+z = 10
+y = 0
+x = y < z and z > y or y > z and z < y
+print(x)
+
+print('Question 9')
+for i in range(1):
+    print("#")
+else:
+    print("#")
+
+print('Question 10')
+a = 1
+b = 0
+c = a & b
+d = a | b
+e = a ^ b
+print(c + d + e)
+
+print('Question 11')
+my_list = [i for i in range(-1,2)]
+print(my_list )
+
+print('Question 12')
+my_list = [1,2,3]
+for v in range(len(my_list)):
+    my_list.insert(1, my_list[v])
+print(my_list)
+
+
+print('Question 14')
+i = 0
+while i <= 5 :
+    i += 1
+    if i % 2 == 0:
+        break
+    print("*")
+
+print('Question 15')
+vals = [0,1,2]
+vals[0], vals[2] = vals[2], vals[0]
+print(vals)
+
+print('Question 16')
+my_list = [1,2,3,4]
+print(my_list[-3:-2])
+
+print('Question 17')
+var = 1
+while var < 10:
+    print("#")
+    var = var << 1
+
+print('Question 18')
+vals = [0,1,2]
+vals.insert(0,1)
+del vals[1]
+print(vals)
+
+print('Question 19')
+x = 1
+x = x == x
+print(x)
+
+print('Question 20')
+my_list_1 = [1,2,3]
+my_list_2 = []
+for v in my_list_1:
+    my_list_2.insert(0,v)
+print(my_list_2)
